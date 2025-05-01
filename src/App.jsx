6cc70b1415089b69910447d662b619c85be938a3
Deepak Kumar <deepaksharma2004kotala@gmail.com>
2025-05-01 15:12:13 -0700

    fix: correct casing for pages/Home path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import Success from "./pages/success";
-import Error from './pages/Error';
+import Error from "./pages/Error";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
